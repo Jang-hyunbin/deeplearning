bed2b6435c81713b3e61abd0ca5963268a32e03a
Jang-hyunbin <47518119+Jang-hyunbin@users.noreply.github.com>
2020-03-27 15:05:47 +0900

    Update low-high_api.py

--- low-high_api.py
+++ low-high_api.py
@@ -1,8 +1,9 @@
+###################low api##################
 W1 = tf.Variable(tf.random_normal([3, 3, 4, 64], stddev=0.01), name = 'pf_w1')
 L1 = tf.nn.conv2d(X_img, W1, strides=[1, 1, 1, 1], padding='SAME', name = 'pf_conv1')
 L1 = tf.nn.relu(L1, name = 'pf_relu1')
 
-##########################################################################################
+###################high api#################
 
 def conv(x_input, num_filter, kernel_size, name_conv1, name_relu1):
     
